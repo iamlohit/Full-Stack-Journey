99dde429c815a27018eab949148601884f5847a4
Lohit <jethwanilohit@gmail.com>
2023-06-24 05:15:20 +0530

    Updated tweeting limit to 140 characters.

---
+++ JS/twitter_char_counter_v2.js
@@ -0,0 +1,14 @@
+// Run this in your browser like this:
+// Chrome > View > Developer > Javascript Console > Sources
+// Look for ">>>" and click "Snippets" and create a new file, named index.js
+// Type the following in the file and hit CMD + Enter (MAC)
+
+var userInput = prompt("Type your tweet here: ");
+var limitedInput = userInput.slice(0,140);
+alert("You have exceeded the character count, the limit is 140 Characters only, here is your message: \n" + limitedInput);
+
+// Enter the following text and see the response:
+// JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.
+// JavaScript's dynamic capabilities include runtime object construction, variable parameter lists, function variables, dynamic script creation (via eval), object introspection (via for...in and Object utilities), and source-code recovery (JavaScript functions store their source text and can be retrieved through toString()).
+// This section is dedicated to the JavaScript language itself, and not the parts that are specific to Web pages or other host environments. For information about APIs that are specific to Web pages, please see Web APIs and DOM.
+
