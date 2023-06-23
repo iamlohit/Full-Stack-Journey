00780e99e23aa1c108e8229758d6160e26b010b2
Lohit <jethwanilohit@gmail.com>
2023-06-23 16:15:20 +0530

    Variables in JS & some string manupulation

---
+++ JS/variables.js
@@ -0,0 +1,15 @@
+var myName = "Lohit";
+// Save's the string or any user input to the variable myName.
+
+var myName = prompt("What is your name?");
+// Prompt's the user with the value in " " and takes the input & stores it in a variable called myName.
+// Type the name of the variable "myName" to get the value on JS console.
+// Reason to call the keyword to be "var" is that its Variable & can be reassinged another value.
+// Note: var is only used when creating the variable.
+// Note: When you want to use or call the variable, you can call it by its name.
+
+alert(myName);
+// This should print data contained in the variable to the output alert window in the chrome(or whatever) broswer.
+
+alert("Hey there, " + myName + " Welcome to the world of JS!");
+// Doing string & variable concatenation.
