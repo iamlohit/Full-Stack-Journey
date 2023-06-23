977b7b1c18168bab5f1184aa079eaa1adcbefedb
Lohit <jethwanilohit@gmail.com>
2023-06-23 15:54:26 +0530

    Data Types in JS

---
+++ JS/data_types.js
@@ -0,0 +1,12 @@
+// String
+    // Everything within " " is a string.
+alert("Lohit");
+typeof("Lohit");
+
+//  Numbers
+    //  123 No "" needed.
+alert("2+3");
+
+// Boolean
+    // true false also do not need ""
+typeof(true);
