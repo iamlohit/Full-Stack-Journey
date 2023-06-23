15ed44e861996cfd2e46a74bd187bdac8f1f494b
Lohit <jethwanilohit@gmail.com>
2023-06-23 15:39:27 +0530

    My Hello World in JS, Estatic!

---
+++ JS/hello-world-index.js
@@ -0,0 +1,10 @@
+// Run this in your browser like this:
+// Chrome > View > Developer > Javascript Console > Sources
+// Look for ">>>" and click "Snippets" and create a new file, named index.js
+// Type the following in the file and hit CMD + Enter (MAC)
+
+alert("Hello");
+alert("World");
+
+// Watch the magic :)
+// Learn everything you need about JS at https://developer.mozilla.org/en-US/docs/Web/JavaScript
