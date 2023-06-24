6963a2480e9b4124433bde5383ec6142a10e3a8a
Lohit <jethwanilohit@gmail.com>
2023-06-24 20:15:14 +0530

    Be prepared to be embarassed

--- JS Intermediate/adv_bmi_calc_challenge.js
+++ JS Intermediate/adv_bmi_calc_challenge.js
@@ -7,7 +7,6 @@
     // BMI 18.5-24.9, the output should be "Your BMI is <bmi>, so you have a normal weight."
     // BMI >24.9, the output should be "Your BMI is <bmi>, so you are overweight."
     // The message MUST be returned as an output from your function. You should NOT NEED to use alert, prompt or console.log in this challenge.
-    // IMPORTANT the message wording has to match precisely for the code to pass the validation. Including punctuation and capitalisation.
 
     function bmiCalculator (weight, height) {
         var bmi = weight / (height*2);
