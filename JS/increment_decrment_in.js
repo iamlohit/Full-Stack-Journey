770923eca51416d93c1d6c1828ebb00bd8263162
Lohit <jethwanilohit@gmail.com>
2023-06-24 11:18:45 +0530

    Addition & Subtraction expressions and ways

---
+++ JS/increment_decrment_in.js
@@ -0,0 +1,29 @@
+// Run this in your browser like this:
+// Chrome > View > Developer > Javascript Console > Sources
+// Look for ">>>" and click "Snippets" and create a new file, named index.js
+// Type the following in the file and hit CMD + Enter (MAC)
+
+// If you want to increase the value of x by 1, there are 2 ways to go about it:
+var x = 5;
+x = x + 1;
+// or
+x++;
+// Both have a result of 6.
+
+// Same goes for decrementing by 1,
+x = x -1;
+// or
+x--;
+
+// If you want to increase the value of x by more than 1, lets say 2, there are 2 ways again:
+var x = 5;
+x = x + 2;
+// or
+x += 2;
+// Both have a result of 7.
+
+// Same goes for decrementing by 2,
+x = x - 2;
+// or
+x -= 2;
+// Both have a result of 3.
