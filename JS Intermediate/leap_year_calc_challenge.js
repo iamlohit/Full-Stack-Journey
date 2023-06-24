acaa136f9a53cfaf4ebc96d6f5c191506c8ed5ab
Lohit <jethwanilohit@gmail.com>
2023-06-25 05:11:47 +0530

    Checking my IF/ELSE IF/ELSE logic in JS: Sounds good! :)

--- JS Intermediate/leap_year_calc_challenge.js
+++ JS Intermediate/leap_year_calc_challenge.js
@@ -3,7 +3,7 @@
     // 💪This is a Difficult Challenge 💪
     // Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February.
     // This is how to work out whether if a particular year is a leap year:
-    
+
         // A year is a leap year if it is evenly divisible by 4 ;
         // except if that year is also evenly divisible by 100;
         // unless that year is also evenly divisible by 400.
@@ -28,4 +28,14 @@ function isLeap(year) {
             else if (year % 100 !== 0){result = "Leap year."}
         else if (year % 400 !== 0){result = "Not leap year."}
         return result;
+    }
+
+// Another way to write this could be, (without using a variable)
+function isLeap(year) {
+        if (year % 4 === 0) 
+            if (year % 100 === 0) 
+                if (year % 400 === 0) return "Leap year."
+                else return "Not leap year.";
+            else return "Leap year.";
+        else result = "Not leap year.";
     }
