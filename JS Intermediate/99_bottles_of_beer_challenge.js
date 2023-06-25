621f85e3e2e5577f695fd8787d33db275d18063e
Lohit <jethwanilohit@gmail.com>
2023-06-25 12:38:12 +0530

    Topsy turvy after so much beer! :@

---
+++ JS Intermediate/99_bottles_of_beer_challenge.js
@@ -0,0 +1,8 @@
+var bottles = 99
+    while (bottles >= 1) {
+        console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+        bottles--;
+        console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.\n");
+    }
+    console.log("No more bottles of beer on the wall, no more bottles of beer.")
+    console.log("Go to the store and buy some more, " + bottles + " of bottles of beer on the wall.\n")
