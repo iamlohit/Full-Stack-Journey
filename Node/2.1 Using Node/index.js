d66798f8e5eb4bd76b7a039b425cf1cfe85c43ae
Lohit <jethwanilohit@gmail.com>
2023-06-26 07:21:56 +0530

    Moving from Chrome V8 to Node runtime for JS

---
+++ Node/2.1 Using Node/index.js
@@ -0,0 +1,8 @@
+// So far we have been using JS in the Chrome browser's V8 Engine which is a JS runtime environment.
+// Going forward as we lean into backend development, we are going to be working with Node JS runtime.
+// Node is what brought the JS engine out of the browser lock and into the backend.
+
+// Install Node LTS Runtime for mac. (Verify using "node -v" on your terminal)
+// Run this file using the command "node index.js" from the folder containing the file.
+
+console.log("Welcome to Node");
