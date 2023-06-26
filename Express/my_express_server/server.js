82253710084d6a979b81bd595dab6d16abcf7acf
Lohit <jethwanilohit@gmail.com>
2023-06-26 16:40:35 +0530

    Adding different HTTP Get Routes or Pages to the website

--- Express/my_express_server/server.js
+++ Express/my_express_server/server.js
@@ -5,13 +5,22 @@ const app = express();
 app.get("/", function(req, res){
     res.send("<h1>Hello, world!</h1>");
 });
-// Here we instruct that when we recieve a "GET" request at the "/" location,
+// Here we instruct that when we recieve a "GET" request at the "/" location, or the Homepage.
 // Use the request(req for short) and response(res) functions,
 // Of which there are many methods available,
 // and we use the res.send (the response we are sending against the GET request to "/"),
 // where we send this HTML text that the browser will Render/Interpret and display to the user.
+// This is a "Route" for "/" and this gets calledback when the user hits the "/" route.
 
+app.get("/contact", function(req, res){
+    res.send("Contact me at: jethwanilohit@gmail.com")
+});
+// This is a "Route" for "/contact" page and this gets called back when the user hits the "/contact" route.
 
+app.get("/about", function(req, res){
+    res.send("<h1>Driven by curiosity, working as an SRE.</h1>")
+});
+// This is a "Route" for "/about" page and this gets called back when the user hits the "/about" route.
 
 // Configure the HTTP server to listen on to port 3000
 app.listen(3000, function(){
