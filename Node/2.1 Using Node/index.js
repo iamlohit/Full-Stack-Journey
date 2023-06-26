9eb60644adfaba1d21256903111124efcbe76581
Lohit <jethwanilohit@gmail.com>
2023-06-26 07:28:00 +0530

    Moving from Chrome V8 to Node runtime for JS

--- Node/2.1 Using Node/index.js
+++ Node/2.1 Using Node/index.js
@@ -7,5 +7,6 @@
 
 // The thing about Node is that so far, with JS, we have been running the JS code sequentially, line by line. i.e Synchronously.
 // With Node, we enter the asyncronous, event-driven, JS runtime, that is designed to build scalable network applications.
+// We are going to use the Node's - Read Evaluate Print Loop or REPL - where we will create JS code and let node run it.
 
 console.log("Welcome to Node");
