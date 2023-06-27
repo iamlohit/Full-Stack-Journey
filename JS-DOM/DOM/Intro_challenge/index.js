c8666d5d1f6444d9e16300cc33c860f5c1ebc682
Lohit <jethwanilohit@gmail.com>
2023-06-27 12:49:09 +0530

    Understood DOM and query selection and modification

--- JS-DOM/DOM/Intro_challenge/index.js
+++ JS-DOM/DOM/Intro_challenge/index.js
@@ -1,3 +1,3 @@
-document.query
 var listItem = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
-listItem.innerHTML="I got this."
+listItem.innerHTML="I got this.";
+document.querySelector("button").style.backgroundColor = "yellow";
