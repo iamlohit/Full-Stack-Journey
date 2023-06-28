d5d6b65bff4bb0fdb6d15278929d0c8a9d964583
Lohit <jethwanilohit@gmail.com>
2023-06-28 07:23:28 +0530

    Man i loved making this, go on and play!

---
+++ JS-DOM/DOM/Dice_game_challenge/index.js
@@ -0,0 +1,12 @@
+let randomNumber1 = Math.floor(Math.random()*6+1);
+let randomNumber2 = Math.floor(Math.random()*6+1);
+
+let image1 = document.querySelector(".dice .img1");
+let image2 = document.querySelector(".dice .img2");
+
+image1.setAttribute("src", `images/dice${randomNumber1}.png`);
+image2.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+if (randomNumber1 === randomNumber2) {document.querySelector("h1").innerHTML = "Draw!"}
+else if (randomNumber1 > randomNumber2) {document.querySelector("h1").innerHTML = "Player 1 Wins!"}
+else if (randomNumber1 < randomNumber2) {document.querySelector("h1").innerHTML = "Player 2 Wins!"};
