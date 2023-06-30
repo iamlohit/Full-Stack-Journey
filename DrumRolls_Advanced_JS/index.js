468c6a43865439d18e2042104859b0788c94cd23
Lohit <jethwanilohit@gmail.com>
2023-06-30 07:22:29 +0530

    Learned about JS Switches and .this, so many variations! Wuff

--- DrumRolls_Advanced_JS/index.js
+++ DrumRolls_Advanced_JS/index.js
@@ -1,10 +1,49 @@
 var buttons = document.querySelectorAll(".drum");
 
 for (var i=0; i<buttons.length; i++) {
-    buttons[i].addEventListener("click", handleClick);
-    }
+    buttons[i].addEventListener("click", function () {
 
-function handleClick() {
-    var audio = new Audio("./sounds/tom-1.mp3");
-    return audio.play();
-}
+    var buttonInnerHTML = this.innerHTML.trim();
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+        break;
+        
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+        break;
+        
+        case "l":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+        break;
+
+        default: console.log(buttonInnerHTML);
+        break;
+
+    }
+    });
+    }
