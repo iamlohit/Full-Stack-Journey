d6ba00dbaa703ac5fc121397458a29c6df79873c
Lohit <jethwanilohit@gmail.com>
2023-07-01 07:07:47 +0530

    Advanced Dom and Callbacks

--- DrumRolls_Advanced_JS/index.js
+++ DrumRolls_Advanced_JS/index.js
@@ -4,12 +4,14 @@ for (var i=0; i<buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML)
     }
     );
 }
 
     document.addEventListener("keydown", function (event) {
         makeSound(event.key);
+        buttonAnimation(event.key)
     });
 
     function makeSound(key) {
@@ -52,3 +54,11 @@ for (var i=0; i<buttons.length; i++) {
             default: console.log(key);
             break;
     }}
+
+    function buttonAnimation(currentKey) {
+        var activeButton = document.querySelector("." + currentKey)
+        activeButton.classList.add("pressed");
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
