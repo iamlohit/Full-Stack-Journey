846d68dc196372fb51920231164f485289f3a829
Lohit <jethwanilohit@gmail.com>
2023-07-01 09:24:38 +0530

    Even easier method `on` to add event listeners with jQ

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,6 +2,10 @@
 //     $("h1").css("color", "purple");
 // });
 
-$(document).keypress(function (event){
-    $("h1").text(event.key);
+// $(document).keypress(function (event){
+//     $("h1").text(event.key);
+// });
+
+$("h1").on("click", function() {
+    $("h1").css("color", "red");
 });
